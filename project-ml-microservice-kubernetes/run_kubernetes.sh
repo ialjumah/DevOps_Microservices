13dedfbe0774ab71e8a7acde1888b629ff9e156c
EC2 Default User <ec2-user@ip-172-31-75-159.ec2.internal>
2021-11-19 21:27:43 +0000

    edited and run make predi tions

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
-
-# This tags and uploads an image to Docker Hub
-
-# Step 1:
-# This is your Docker ID/path
-# dockerpath=<>
-
-# Step 2
+dockerpath=ibdiko/idt
 # Run the Docker Hub container with kubernetes
+#kubectl run container --image=$dockerpath --image-pull-policy="Always"
+kubectl create deployment udacityproject --image=$dockerpath
+
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
+
+sleep 5
 
 # Step 4:
 # Forward the container port to a host
+
+kubectl port-forward udacityproject-756b6b9dc7-jx7jw 8080:80
 
